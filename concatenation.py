533f90b4969ce130ee8396e2513e162e23610dc0
Shreyas5347 <rajputshreyas321@gmail.com>
2026-01-30 06:34:56 +0000

    Initial commit

---
+++ concatenation.py
@@ -0,0 +1,7 @@
+'''print("hello"+ name+"my age is "+age)
+this will generate an error that 
+integer (age) cannot concante with string 
+so we will convert it into integer by str(age)'''
+name="shreyas"
+age=20
+print("hello my name is "+ name+" and my age is "+str(age))
